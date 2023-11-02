6497e35db4ff9c75cc5a0e62353e2e0e233c43a7
unknown <you@example.com>
2023-11-02 23:00:52 +0300

    refactor: refactor types, intial loading, brought commit into compliance with the requirements

--- src/components/ResultItem/ResultItem.tsx
+++ src/components/ResultItem/ResultItem.tsx
@@ -11,15 +11,16 @@ export class ResultItem extends Component<PlanetData> {
       <div
         style={{
           display: 'flex',
-          height: 'max-content',
           flexDirection: 'column',
           backgroundColor: 'red',
+          padding: '20px',
+          borderRadius: '16px',
         }}
       >
         <p>{this.props.name}</p>
         <p>{this.props.population}</p>
         <p>{this.props.climate}</p>
-        {/* <p>{this.props.terrain}</p> */}
+        <p>{this.props.terrain}</p>
       </div>
     );
   }
